3d818260531e2f75bdb5a6e3f1e40fef1ca7a278
Redacted <155997486+R3d4ccted@users.noreply.github.com>
2025-08-15 18:41:26 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,42 @@
 { config, pkgs, ... }:
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
+  system.stateVersion = "25.05";
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  networking.hostName = "nixos-vm";
+  networking.hostName = "nixos"; # ubah kalau mau
   networking.networkmanager.enable = true;
   time.timeZone = "Asia/Jakarta";
   i18n.defaultLocale = "en_US.UTF-8";
-  console.keyMap = "us";
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  users.users.nixos = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    password = "nixos";
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
   };
+  users.users.user = {
+    isNormalUser = true;
+    description = "kurond";
+    extraGroups = [ "wheel" "networkmanager" ]; # sudo + network access
+    password = "12345"; 
+  };
+  security.sudo.wheelNeedsPassword = false;
   environment.systemPackages = with pkgs; [
-    vim
     firefox
-    git
-    wget
-    curl
-    neofetch
-    wine
-    steam
     vscode
+    git
+    curl
+    wget
+    gnome.gnome-terminal
+    gnome.dconf-editor
   ];
-  security.sudo.enable = true;
   hardware.opengl.enable = true;
-  virtualisation.vmware.guest.enable = true;
+  services.printing.enable = true;
+  services.flatpak.enable = true;
+  networking.firewall.enable = true;
 }
